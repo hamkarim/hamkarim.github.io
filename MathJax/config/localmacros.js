0dcd576e9c6a9c4bb174246f0e932222bb84d678
hamkarim <hamkarim@gmail.com>
2020-07-04 00:07:03 +0700

    Initial commit

---
+++ MathJax/config/localmacros.js
@@ -0,0 +1,34 @@
+// <script type="text/x-mathjax-config"> //
+  MathJax.Hub.Config({
+    TeX: {
+        Macros: {
+          RR: "{\\bf R}",
+          E: "{\\bf E}",
+          F: "{\\bf F}",
+          G: "{\\bf G}",
+          H: "{\\bf H}",
+          calE: "{\\mathcal E}",
+          calF: "{\\mathcal F}",
+          calG: "{\\mathcal G}",
+          calH: "{\\mathcal H}",
+          R: "{\\mathbb R}",
+          Cc: "{\\mathbb C}",
+          bold: ["{\\bf #1}",1],
+          bfseries: ["{\\bf{\\text{ #1 }}",1],
+          fontbold: ["{\\bf{\\text{ #1 }}}",1],
+          Abs: ['\\left\\lvert #2 \\right\\rvert_{\\text{#1}}', 2, ""],
+          vect: ["{\\overrightarrow{#1}}",1],
+        }
+    },
+    tex2jax: {
+      inlineMath: [ ['$','$'], ["\\(","\\)"] ],
+      processEscapes: true,
+      equationNumbers: { autoNumber: "AMS" }
+    }
+  });
+
+MathJax.Ajax.loadComplete("https://hamkarim.github.io/MathJax/config/local/local.js");
+
+
+
+//</script> //
