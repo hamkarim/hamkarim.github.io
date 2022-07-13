2a3f05abdef0638efe915f17bd4c5c56e6b63381
hamkarim <hamkarim@gmail.com>
2022-07-13 23:25:48 +0700

    mathJax

--- MathJax/config/local/local.js
+++ MathJax/config/local/local.js
@@ -36,7 +36,7 @@
              linebreaks: { automatic: true } ,
      //scale:90
           },
-      CommonHTML: { scale: 100, linebreaks: { automatic: true } },
+      CommonHTML: { scale: 70, linebreaks: { automatic: true } },
       //CommonHTML: {scale: 20},
       SVG: { linebreaks: { automatic:true } },
     /*tex2jax: {
